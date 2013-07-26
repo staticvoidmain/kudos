cdb3f700cf535fe7fd44b3d90e39b51e2edf6b9d
staticvoidmain <themissinglinq@gmail.com>
2013-07-26 15:50:43 -0500

    added ravendb stuff

---
+++ Kudos/Kudos.Data.Test/KudosRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using Kudos.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Kudos.Data.Test
+{
+	[TestClass]
+	public class KudosRepositoryTest
+	{
+		public TestContext TestContext { get; set; }
+		
+		[TestMethod]
+		public void FindUser_Exact_Match_Returns_Single_Match_User()
+		{
+			KudosRepository target = new KudosRepository(); // TODO: Initialize to an appropriate value
+			var actual = target.FindUser("Ross Jennings");
+
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		public void FindUser_Incomplete_Returns_Suggested_Users()
+		{
+			KudosRepository target = new KudosRepository(); // TODO: Initialize to an appropriate value
+			var actual = target.FindUser("Ross Jennings");
+
+			Assert.Fail();
+		}
+	}
+}
---
+++ Kudos/Kudos.Data/KudosRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+
+namespace Kudos.Data
+{
+	// todo: MapReduce task for gathering point values for a user.
+	// todo: 
+
+	public class KudosRepository
+	{
+		private static readonly DocumentStore documentStore; 
+
+		static KudosRepository()
+		{
+			documentStore = new EmbeddableDocumentStore()  
+			{
+				DataDirectory = "/app_data/db"
+			};
+
+			documentStore.Initialize();
+		}
+
+		// init RavenDB repo here.
+
+		// todo: text search for users.
+		public object FindUser(string name)
+		{
+			using (var session = documentStore.OpenSession())
+			{
+				
+			}
+
+			throw new NotImplementedException("todo: all the things!");
+		}
+	}
+}
